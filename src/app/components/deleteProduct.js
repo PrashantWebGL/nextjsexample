8ab68e88265949ade5b503651de794b695605d74
PrashantWebGL <prashant.dh12@gmail.com>
2024-02-09 18:55:10 +0900

    Corrected delete API path

--- src/app/components/deleteProduct.js
+++ src/app/components/deleteProduct.js
@@ -1,11 +1,12 @@
 "use client"
 
+import { baseURL } from "@/lib/db";
 import { useRouter } from "next/navigation";
 
 export default function DeleteProduct(params){
     const router = useRouter();
     const deleteRecord= async ()=>{
-         let result = await fetch("http://localhost:3000/api/products/" + params.params, {
+         let result = await fetch(baseURL+"/api/products/" + params.params, {
              method: "DELETE",
        
         });
